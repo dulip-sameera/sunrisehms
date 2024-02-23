16b87220e433f0704431a6884a819d94cd652982
Dulip Sameeera <dsgitrepo101@gmail.com>
2024-02-23 14:03:19 +0530

    correct spelling in first_name field

--- src/com/sunrisehms/entity/embedded/UserName.java
+++ src/com/sunrisehms/entity/embedded/UserName.java
@@ -12,7 +12,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class UserName {
     
-    @Column(name = "fist_name", nullable = false)
+    @Column(name = "first_name", nullable = false)
     private String firstName;
     
     @Column(name = "last_name", nullable = false)
