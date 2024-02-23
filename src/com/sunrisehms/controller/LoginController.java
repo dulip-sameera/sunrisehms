19752daee28d8d222dbe340634c3cc9bb5cc9b3b
Dulip Sameeera <dsgitrepo101@gmail.com>
2024-02-24 02:52:59 +0530

    create LoginView

---
+++ src/com/sunrisehms/controller/LoginController.java
@@ -0,0 +1,65 @@
+
+package com.sunrisehms.controller;
+
+import com.sunrisehms.dto.UserDto;
+import com.sunrisehms.dto.UserLoginDto;
+import com.sunrisehms.enums.ServiceType;
+import com.sunrisehms.exception.WrongPassword;
+import com.sunrisehms.service.ServiceFactory;
+import com.sunrisehms.service.custom.LoginService;
+import java.net.URL;
+import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class LoginController implements Initializable{
+    
+    LoginService loginService = (LoginService)ServiceFactory.getInstance().getService(ServiceType.LOGIN);
+
+    @FXML
+    private Button btnLogin;
+
+    @FXML
+    private PasswordField txtPassword;
+
+    @FXML
+    private TextField txtUserName;
+    
+    @FXML
+    private Label lblMessage;
+    
+     @FXML
+    public void login(ActionEvent event) {
+        String name = txtUserName.getText();
+        String password = txtPassword.getText();
+        UserLoginDto dto = new UserLoginDto(name, password);
+        try {
+            UserDto user = loginService.login(dto);
+            if (user == null) {
+                lblMessage.setText("Wrong user name!");
+                lblMessage.setVisible(true);
+                return;
+            }
+            lblMessage.setVisible(false);
+        } catch (WrongPassword wp) {
+            lblMessage.setText("Incorrect password!");
+            lblMessage.setVisible(true);
+        } catch (Exception ex) {
+            Logger.getLogger(LoginController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        lblMessage.setVisible(false);
+    }
+
+}
+
